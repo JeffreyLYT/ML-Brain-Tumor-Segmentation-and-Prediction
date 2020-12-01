d1170827e1d2cff0d81df9469fe69013ee9ac070
Jeffrey Lam <jeffrey.lyt@gmail.com>
2020-11-30 22:17:52 -0500

    initial commit

---
+++ index.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import torch
+import sklearn
+import sklearn.cluster         # For KMeans class
+import sklearn.mixture         # For GaussianMixture class
+import sklearn.preprocessing   # For scale function
+import mpl_toolkits.mplot3d    # For enabling projection='3d' feature in Matplotlib
+
+image = plt.imread('TumorIdentifier/yes/Y1,jpg')
+print(image.shape)
