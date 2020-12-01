3c0e05e994e7c0578f38d7be53bae7f5a61a35fe
Jeffrey Lam <jeffrey.lyt@gmail.com>
2020-11-30 22:47:48 -0500

    temp gray scale

--- index.py
+++ index.py
@@ -8,8 +8,13 @@ import sklearn.mixture         # For GaussianMixture class
 import sklearn.preprocessing   # For scale function
 import mpl_toolkits.mplot3d    # For enabling projection='3d' feature in Matplotlib
 
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+
 image = plt.imread('TumorIdentifier/yes/Y1.jpg')
-print(image.shape)
-plt.imshow(image)
+gray = rgb2gray(image)
+plt.imshow(gray, cmap='gray')
 plt.show(block=False)
 input('press <ENTER> to continue')
+print(gray.shape)
+print(gray)
