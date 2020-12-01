baba4058f8bffcc63c5511b958d91e65cc56fbba
Jeffrey Lam <jeffrey.lyt@gmail.com>
2020-11-30 22:27:48 -0500

    output test

--- index.py
+++ index.py
@@ -8,5 +8,8 @@ import sklearn.mixture         # For GaussianMixture class
 import sklearn.preprocessing   # For scale function
 import mpl_toolkits.mplot3d    # For enabling projection='3d' feature in Matplotlib
 
-image = plt.imread('TumorIdentifier/yes/Y1,jpg')
+image = plt.imread('TumorIdentifier/yes/Y1.jpg')
 print(image.shape)
+plt.imshow(image)
+plt.show(block=False)
+input('press <ENTER> to continue')
